85a679bf4fcbe75ca4924d38153ecdad9152e544
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2024-02-20 17:04:47 -0700

    Added S3 backend for Terraform AWS

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 4.16"
     }
   }
+  backend "s3" {
+    bucket = "hydroserver-terraform-backend"
+    key    = "state"
+    region = "us-east-1"
+  }
   required_version = ">= 1.2.0"
 }
 
