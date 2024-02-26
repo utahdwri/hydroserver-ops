2f723b47f2347c8f98dfb0fba004661c3127ef7d
Kenneth Lippold <kjlippold@outlook.com>
2024-02-26 13:51:19 -0700

    Update main.tf

--- terraform/timescale/main.tf
+++ terraform/timescale/main.tf
@@ -48,3 +48,13 @@ output "password" {
   value=timescale_service.hydroserver_timescale.password
   sensitive = true
 }
+
+output "connection_string" {
+  value = format(
+    "postgresql://tsdbadmin:%s@%s:%s/tsdb",
+    timescale_service.hydroserver_timescale.password,
+    timescale_service.hydroserver_timescale.hostname,
+    timescale_service.hydroserver_timescale.port
+  )
+  sensitive = true
+}
