0a53f121a9bb7b67b49c468f25dc17c71184d35f
Kenneth Lippold <kjlippold@outlook.com>
2024-02-27 12:06:29 -0700

    Update s3.tf to combine static and storage buckets

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -6,16 +6,12 @@ resource "aws_s3_bucket" "hydroserver_web_bucket" {
   bucket = "hydroserver-${var.instance}-web"
 }
 
-resource "aws_s3_bucket" "hydroserver_static_bucket" {
-  bucket = "hydroserver-${var.instance}-static"
-}
-
 resource "aws_s3_bucket" "hydroserver_storage_bucket" {
   bucket = "hydroserver-${var.instance}-storage"
 }
 
-resource "aws_s3_bucket" "hydroserver_backend_bucket" {
-  bucket = "hydroserver-${var.instance}-backend"
+resource "aws_s3_bucket" "hydroserver_django_bucket" {
+  bucket = "hydroserver-${var.instance}-django"
 }
 
 # ------------------------------------------------ #
@@ -24,15 +20,6 @@ resource "aws_s3_bucket" "hydroserver_backend_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "hydroserver_web_bucket" {
   bucket = aws_s3_bucket.hydroserver_web_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_public_access_block" "hydroserver_static_bucket" {
-  bucket = aws_s3_bucket.hydroserver_static_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -49,8 +36,8 @@ resource "aws_s3_bucket_public_access_block" "hydroserver_storage_bucket" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_public_access_block" "hydroserver_backend_bucket" {
-  bucket = aws_s3_bucket.hydroserver_backend_bucket.id
+resource "aws_s3_bucket_public_access_block" "hydroserver_django_bucket" {
+  bucket = aws_s3_bucket.hydroserver_django_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -68,14 +55,6 @@ resource "aws_s3_bucket_ownership_controls" "hydroserver_web_bucket" {
     object_ownership = "BucketOwnerEnforced"
   }
   depends_on = [aws_s3_bucket_public_access_block.hydroserver_web_bucket]
-}
-
-resource "aws_s3_bucket_ownership_controls" "hydroserver_static_bucket" {
-  bucket = aws_s3_bucket.hydroserver_static_bucket.id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.hydroserver_static_bucket]
 }
 
 resource "aws_s3_bucket_ownership_controls" "hydroserver_storage_bucket" {
@@ -130,35 +109,6 @@ resource "aws_s3_bucket_policy" "hydroserver_web_bucket" {
   ]
 }
 
-resource "aws_s3_bucket_policy" "hydroserver_static_bucket" {
-  bucket = aws_s3_bucket.hydroserver_static_bucket.id
-  policy = jsonencode({
-    Version = "2008-10-17"
-    Id      = "PolicyForCloudFrontPrivateContent"
-    Statement = [
-      {
-        Sid    = "AllowCloudFrontServicePrincipal"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.hydroserver_static_bucket.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.hydroserver_distribution.arn
-          }
-        }
-      }
-    ]
-  })
-
-  depends_on = [
-    aws_cloudfront_distribution.hydroserver_distribution,
-    aws_s3_bucket_public_access_block.hydroserver_static_bucket
-  ]
-}
-
 resource "aws_s3_bucket_policy" "hydroserver_storage_bucket" {
   bucket = aws_s3_bucket.hydroserver_storage_bucket.id
   policy = jsonencode({
@@ -186,4 +136,18 @@ resource "aws_s3_bucket_policy" "hydroserver_storage_bucket" {
     aws_cloudfront_distribution.hydroserver_distribution,
     aws_s3_bucket_public_access_block.hydroserver_storage_bucket
   ]
+}
+
+# ------------------------------------------------ #
+# HydroServer S3 Storage Folders                   #
+# ------------------------------------------------ #
+
+resource "aws_s3_bucket_object" "media_folder" {
+  bucket = aws_s3_bucket.hydroserver_storage_bucket.id
+  key    = "media/"
+}
+
+resource "aws_s3_bucket_object" "static_folder" {
+  bucket = aws_s3_bucket.hydroserver_storage_bucket.id
+  key    = "static/"
 }
