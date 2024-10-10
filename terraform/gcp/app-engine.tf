1a1518086be5315474b2c7a4a4a9daf670784baa
Kenneth Lippold <kjlippold@outlook.com>
2024-10-10 15:11:34 -0700

    Create app-engine.tf

---
+++ terraform/gcp/app-engine.tf
@@ -0,0 +1,56 @@
+# ------------------------------------------------ #
+# HydroServer Google App Engine Service            #
+# ------------------------------------------------ #
+resource "google_app_engine_standard_app_version" "hydroserver_django_service" {
+  service                = "hydroserver-${var.instance}"
+  version_id             = "v0"
+  runtime                = "python311"
+  entrypoint {
+    shell = "gunicorn -b :$PORT hydroserver.wsgi"
+  }
+
+  # ------------------------------ #
+  # Scaling Configuration          #
+  # ------------------------------ #
+  
+  automatic_scaling {
+    max_instances = 1
+    min_idle_instances = 0
+  }
+
+  # ------------------------------ #
+  # Environment Variables          #
+  # ------------------------------ #
+
+  env_variables = {
+    "ADMIN_EMAIL"              = ""
+    "ALLOWED_HOSTS"            = ""
+    "AWS_STORAGE_BUCKET_NAME"  = ""
+    "DATABASE_URL"             = ""
+    "DEBUG"                    = "True"
+    "DEPLOYMENT_BACKEND"       = "gcp"
+    "DISABLE_ACCOUNT_CREATION" = "False"
+    "EMAIL_HOST"               = ""
+    "EMAIL_PORT"               = ""
+    "EMAIL_HOST_USER"          = ""
+    "EMAIL_HOST_PASSWORD"      = ""
+    "OAUTH_GOOGLE_CLIENT"      = ""
+    "OAUTH_GOOGLE_SECRET"      = ""
+    "OAUTH_HYDROSHARE_CLIENT"  = ""
+    "OAUTH_HYDROSHARE_SECRET"  = ""
+    "OAUTH_ORCID_CLIENT"       = ""
+    "OAUTH_ORCID_SECRET"       = ""
+    "PROXY_BASE_URL"           = ""
+    "SECRET_KEY"               = ""
+  }
+
+  handlers {
+    url_regex = ".*"
+    script    = "auto"
+  }
+
+  resources {
+    cpu    = 1
+    memory = "512MB"
+  }
+}
