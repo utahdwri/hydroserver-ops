2fa09204f45f1858b913172091fd5bde1cedc92e
Kenneth Lippold <kjlippold@outlook.com>
2024-10-10 10:34:51 -0700

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,8 +1,21 @@
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
   backend "gcs" {}
   required_version = ">= 1.2.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
 variable "instance" {}
 variable "project_id" {}
 variable "region" {}
+
+data "google_client_config" "current" {}
