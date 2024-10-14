460feb1e5482794458e246515953bbfcc9f278af
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 14:54:18 -0700

    Update cloud-cdn.tf

--- terraform/gcp/cloud-cdn.tf
+++ terraform/gcp/cloud-cdn.tf
@@ -10,15 +10,23 @@ resource "google_compute_backend_service" "hydroserver_backend" {
     group = google_compute_region_backend_service.hydroserver_service.self_link
   }
 
-  # Enable Cloud CDN for the backend service
   enable_cdn = true
 
-  # Configure caching
   cdn_policy {
     cache_mode        = "CACHE_ALL_STATIC"
     default_ttl      = 3600
     max_ttl          = 86400
     min_ttl          = 60
     signed_url_cache_mode = "USE_CACHED"
+  }
+}
+
+resource "google_compute_region_backend_service" "hydroserver_service" {
+  name        = "hydroserver-service-backend"
+  port_name   = "http"
+  protocol    = "HTTP"
+
+  backend {
+    group = google_cloud_run_service.hydroserver_api.id
   }
 }
