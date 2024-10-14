90e6e39e0624811856f61d81aadfa82a4b45bb8c
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 10:32:14 -0700

    Update cloud-run.tf

--- terraform/gcp/cloud-run.tf
+++ terraform/gcp/cloud-run.tf
@@ -2,46 +2,39 @@
 # HydroServer GCP Cloud Run Service                  #
 # -------------------------------------------------- #
 
-#resource "google_cloud_run_service" "hydroserver_api" {
-#  name     = "hydroserver-api-${var.instance}"
-#  location = var.region
+resource "google_cloud_run_service" "hydroserver_api" {
+  name     = "hydroserver-api-${var.instance}"
+  location = var.region
 
-#  template {
-#    spec {
-#      containers {
-#        image = "gcr.io/cloudrun/hello"
-#        resources {
-#          limits = {
-#            memory = "512Mi"
-#          }
-#        }
-#        ports {
-#          container_port = 8080
-#        }
-#      }
-#    }
-#  }
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+        resources {
+          limits = {
+            memory = "512Mi"
+          }
+        }
+        ports {
+          container_port = 8080
+        }
+      }
+    }
+  }
 
-#  traffic {
-#    percent         = 100
-#    latest_revision = true
-#  }
-#}
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
 
 # -------------------------------------------------- #
 # HydroServer GCP Cloud Run Service Public Access    #
 # -------------------------------------------------- #
 
-#resource "google_cloud_run_service_iam_member" "hydroserver_api_public_access" {
-#  service  = google_cloud_run_service.hydroserver_api.name
-#  location = google_cloud_run_service.hydroserver_api.location
-#  role     = "roles/run.invoker"
-#  member   = "allUsers"
-#}
-
-resource "google_storage_bucket" "test_bucket" {
-  name     = "hydroserver-test-bucket"
-  location = var.region
-  force_destroy = true
-  uniform_bucket_level_access = true
+resource "google_cloud_run_service_iam_member" "hydroserver_api_public_access" {
+  service  = google_cloud_run_service.hydroserver_api.name
+  location = google_cloud_run_service.hydroserver_api.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
