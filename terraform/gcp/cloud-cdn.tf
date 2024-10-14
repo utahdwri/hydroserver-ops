cb8a0061a1e72c47669b820193f1d64af803b5fd
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 14:59:15 -0700

    Update cloud-cdn.tf

--- terraform/gcp/cloud-cdn.tf
+++ terraform/gcp/cloud-cdn.tf
@@ -16,6 +16,11 @@ resource "google_compute_backend_service" "hydroserver_backend" {
     cache_mode        = "CACHE_ALL_STATIC"
     default_ttl      = 3600
     max_ttl          = 86400
+    cache_key_policy {
+      include_host          = true
+      include_protocol      = true
+      include_query         = false
+    }
   }
 }
 
