9f660e7f62e2b6b5952d45d7e45cd6087f9ef534
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 16:30:39 -0700

    Update cloud-cdn.tf

--- terraform/gcp/cloud-cdn.tf
+++ terraform/gcp/cloud-cdn.tf
@@ -1,6 +1,11 @@
 # -------------------------------------------------- #
-# Google Cloud CDN Configuration for Cloud Run      #
+# Google Cloud CDN Configuration for Cloud Run       #
 # -------------------------------------------------- #
+
+resource "google_compute_global_address" "hydroserver_lb_ip" {
+  name = "hydroserver-lb-ip-${var.instance}"
+}
+
 
 resource "google_compute_backend_service" "hydroserver_backend" {
   name                  = "hydroserver-backend-${var.instance}"
@@ -22,4 +27,26 @@ resource "google_compute_backend_service" "hydroserver_backend" {
       include_protocol      = true
     }
   }
+}
+
+resource "google_compute_url_map" "hydroserver_url_map" {
+  name            = "hydroserver-url-map-${var.instance}"
+  default_service = google_compute_backend_service.hydroserver_backend.id
+}
+
+resource "google_compute_target_http_proxy" "hydroserver_http_proxy" {
+  name   = "hydroserver-http-proxy-${var.instance}"
+  url_map = google_compute_url_map.hydroserver_url_map.id
+}
+
+resource "google_compute_global_forwarding_rule" "hydroserver_forwarding_rule" {
+  name       = "hydroserver-forwarding-rule-${var.instance}"
+  target     = google_compute_target_http_proxy.hydroserver_http_proxy.id
+  port_range = "80"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address = google_compute_global_address.hydroserver_ip.address
+}
+
+resource "google_compute_global_address" "hydroserver_ip" {
+  name = "hydroserver-ip-${var.instance}"
 }
