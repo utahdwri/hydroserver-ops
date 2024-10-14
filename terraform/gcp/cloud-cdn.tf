8c395fc38072393b1222d80df44ab8ea263e4228
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 14:51:50 -0700

    Update cloud-cdn.tf

--- terraform/gcp/cloud-cdn.tf
+++ terraform/gcp/cloud-cdn.tf
@@ -2,60 +2,23 @@
 # Google Cloud CDN Configuration for Cloud Run      #
 # -------------------------------------------------- #
 
-resource "google_compute_global_address" "default" {
-  name = "hydroserver-api-address-${var.instance}"
-}
-
-resource "google_compute_network_endpoint_group" "cloud_run_neg" {
-  name                    = "cloud-run-neg-${var.instance}"
-  network_endpoint_type   = "serverless"
-  network                 = "default"
-  endpoint {
-    url = google_cloud_run_service.hydroserver_api.status[0].url
-  }
-}
-
-resource "google_compute_backend_service" "cloud_run_backend" {
-  name                  = "cloud-run-backend-${var.instance}"
+resource "google_compute_backend_service" "hydroserver_backend" {
+  name                  = "hydroserver-backend-${var.instance}"
   load_balancing_scheme = "EXTERNAL"
 
   backend {
-    group = google_compute_network_endpoint_group.cloud_run_neg.id
+    group = google_compute_region_backend_service.hydroserver_service.self_link
   }
 
-  health_checks = [google_compute_health_check.default.id]
-  timeout_sec  = 10
-  port_name    = "http"
-
+  # Enable Cloud CDN for the backend service
   enable_cdn = true
-}
 
-resource "google_compute_health_check" "default" {
-  name                = "health-check-${var.instance}"
-  check_interval_sec  = 10
-  timeout_sec         = 4
-  healthy_threshold   = 2
-  unhealthy_threshold = 3
-
-  http_health_check {
-    port = 8080
-    request_path = "/health"
+  # Configure caching
+  cdn_policy {
+    cache_mode        = "CACHE_ALL_STATIC"
+    default_ttl      = 3600
+    max_ttl          = 86400
+    min_ttl          = 60
+    signed_url_cache_mode = "USE_CACHED"
   }
-}
-
-resource "google_compute_url_map" "default" {
-  name            = "url-map-${var.instance}"
-  default_service = google_compute_backend_service.cloud_run_backend.id
-}
-
-resource "google_compute_target_http_proxy" "default" {
-  name    = "http-proxy-${var.instance}"
-  url_map = google_compute_url_map.default.id
-}
-
-resource "google_compute_global_forwarding_rule" "default" {
-  name       = "forwarding-rule-${var.instance}"
-  target     = google_compute_target_http_proxy.default.id
-  port_range = "80"
-  ip_address = google_compute_global_address.default.address
 }
