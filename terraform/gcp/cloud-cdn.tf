28388b4d5fd20ab36698b412b9de8a6e4be70da6
Kenneth Lippold <kjlippold@outlook.com>
2024-10-14 15:38:46 -0700

    Update cloud-cdn.tf

--- terraform/gcp/cloud-cdn.tf
+++ terraform/gcp/cloud-cdn.tf
@@ -7,7 +7,7 @@ resource "google_compute_backend_service" "hydroserver_backend" {
   load_balancing_scheme = "EXTERNAL"
 
   backend {
-    group = google_compute_region_backend_service.hydroserver_service.self_link
+    group = google_compute_region_backend_service.hydroserver_service.instance_group
   }
 
   enable_cdn = true
